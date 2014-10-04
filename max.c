ad65cfc541216c0166d935eaaab897796df03f3d
Lisong Guo <lisong.guo@inria.fr>
2014-10-04 00:38:40 -0500

    remove the switch function.

--- max.c
+++ max.c
@@ -1,17 +1,9 @@
 /**
  *  Find the maximum number without using if-else or other comparison operator. 
  * 
- *  Switch two number without using a temporary/third variable.
  **/
 #include<stdio.h>
 #include<stdlib.h>
-
-
-void exchange(int *a, int *b){
-    *a = (*a) - (*b);  // diff 
-    *b = (*b) + (*a);
-    *a = (*b) - (*a); 
-}
 
 
 // int abs(int a){ return (int)((unsigned int)a); }
@@ -32,10 +24,6 @@ int main(){
     int a = 20, b = 30; 
 
     printf("a=%d, b=%d, max=%d\n", a, b, max_1(a,b)); 
-
-    exchange(&a, &b); 
-
-    printf("After exchange: a=%d, b=%d\n", a, b); 
 
     return 0;
 }
